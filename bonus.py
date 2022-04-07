c2be0212ebc9343db9773333f9a5e66837262f28
Ubuntu <ubuntu@ip-172-31-80-220.ec2.internal>
2022-04-07 13:45:51 +0000

    wokred

--- bonus.py
+++ bonus.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # Created by: Hunter Connolly
 # Created on: March 31, 2022
-# This program determines if a number is positive
-# negative or zero
+# This program asks the user or how many years they have worked
+# and if they have worked more than 5 years, they get a 5% bonus
 
 
 import random
@@ -11,21 +11,31 @@ import math
 
 def main():
     # get the number from the user
-    years_worked = float(input("How many years have you been working for?: "))
+    years_worked_string = (input("How many years have you been working for?: "))
+    print("")
 
     # check if number is positive negative or zero
-    if years_worked >= 5:
-        print("CONGRATULATIONS! You are eligible for a bonus this year")
-    elif years_worked == 4:
-        print("Unfortunately you are not eligible for a bonus this year. Next year you will be eligible!")
-    elif years_worked == 3:
-        print("Unfortunately you are not eligible for a bonus this year. In two years you will be eligible")
-    elif years_worked == 2:
-        print("Unfortunately you are not eligible for a bonus this year. In three years you will be eligible")
-    elif years_worked < 5:
-        print("Unfortunately you are not eligible for a bonus this year")
-    else: 
-        print("No idea!")
+    try:
+        years_worked_number = float(years_worked_string)
+        if years_worked_number >= 5:
+            print("CONGRATULATIONS! You are eligible for a bonus this year!!")
+            salary = float(input("Enter your salary: "))
+            print("")
+            bonus = salary * 0.05
+            print("Your bonus this year is: ${:.2f}" .format(bonus))
+            net_bonus = salary + bonus
+            print("Your total salary with the bonus this year is: ${:.2f}"
+            .format(net_bonus))
+        elif years_worked_number == 4:
+            print("Unfortunately you are not eligible for a bonus this year. Next year you will be eligible!")
+        elif years_worked_number == 3:
+            print("Unfortunately you are not eligible for a bonus this year. In two years you will be eligible")
+        elif years_worked_number == 2:
+            print("Unfortunately you are not eligible for a bonus this year. In three years you will be eligible")
+        else:
+            print("Unfortunately you are not eligible for a bonus this year")
+    except Exception:
+        print("That is not a number")
 
 
 if __name__ == "__main__":
